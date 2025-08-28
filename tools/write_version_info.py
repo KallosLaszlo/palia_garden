bcb3b801f3f61131296bf61087330dd1105f5567
kallos.laszlo <kallos.laszlo@gmail.com>
2025-08-28 16:33:30 +0200

    Simplify version_info.py format to PyInstaller text-based format
    - Removed Python imports and variable assignment
    - Using direct PyInstaller text-based representation format
    - No u'' unicode prefixes, cleaner format
    - This should fix the deserialization error once and for all

--- tools/write_version_info.py
+++ tools/write_version_info.py
@@ -22,43 +22,36 @@ def main():
     sha = args.sha
     out_path = args.out
 
-    content = textwrap.dedent(f"""
-    # -*- coding: utf-8 -*-
-    from PyInstaller.utils.win32.versioninfo import VSVersionInfo, FixedFileInfo, StringFileInfo, StringTable, StringStruct, VarFileInfo, VarStruct
+    content = f"""# -*- coding: utf-8 -*-
+# Version information for PyInstaller
 
-    version_info = VSVersionInfo(
-      ffi=FixedFileInfo(
-        filevers=(1,0,0,0),
-        prodvers=(1,0,0,0),
+VSVersionInfo(
+    ffi=FixedFileInfo(
+        filevers=(1, 0, 0, 0),
+        prodvers=(1, 0, 0, 0),
         mask=0x3f,
         flags=0x0,
         OS=0x40004,
         fileType=0x1,
         subtype=0x0,
         date=(0, 0)
-      ),
-      kids=[
-        StringFileInfo(
-          [
-            StringTable(
-              u'040904B0',
-              [
-                StringStruct(u'CompanyName', u'Palia Garden Optimizer'),
-                StringStruct(u'FileDescription', u'Palia Garden Optimizer'),
-                StringStruct(u'FileVersion', u'{tag}'),
-                StringStruct(u'InternalName', u'PaliaGardenOptimizer'),
-                StringStruct(u'LegalCopyright', u'Open Source'),
-                StringStruct(u'OriginalFilename', u'PaliaGardenOptimizer.exe'),
-                StringStruct(u'ProductName', u'Palia Garden Optimizer'),
-                StringStruct(u'ProductVersion', u'{tag}'),
-              ]
-            )
-          ]
-        ),
-        VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
-      ]
-    )
-    """).strip()
+    ),
+    kids=[
+        StringFileInfo([
+            StringTable('040904B0', [
+                StringStruct('CompanyName', 'Palia Garden Optimizer'),
+                StringStruct('FileDescription', 'Palia Garden Optimizer'),
+                StringStruct('FileVersion', '{tag}'),
+                StringStruct('InternalName', 'PaliaGardenOptimizer'),
+                StringStruct('LegalCopyright', 'Open Source'),
+                StringStruct('OriginalFilename', 'PaliaGardenOptimizer.exe'),
+                StringStruct('ProductName', 'Palia Garden Optimizer'),
+                StringStruct('ProductVersion', '{tag}'),
+            ])
+        ]),
+        VarFileInfo([VarStruct('Translation', [1033, 1200])])
+    ]
+)"""
 
     if sha:
         content = f"# commit: {sha}\n" + content
